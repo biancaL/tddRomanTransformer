c5d55cc04034f7e153160e6208abd9f5574aa008
Bianca Leuca <bianca.leuca@gmail.com>
2017-05-28 10:50:27 +0300

    Transformer until 19 works

--- src/test/groovy/RomanNumeralTransformerSpec.groovy
+++ src/test/groovy/RomanNumeralTransformerSpec.groovy
@@ -4,41 +4,58 @@ import spock.lang.Unroll
 
 class RomanNumeralTransformerSpec extends Specification {
 
+
 	def arabicToRoman = [1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 9: "IX", 10: "X"]
 
+	def "it should fail"() {
+		expect:
+		true
+	}
+
 	@Unroll
-	def "for given value #arabicNumeral should return #expectedRomanNumeral"() {
+	def "it should return #expectedResult"() {
 		when:
 		def romanNumeral = transform(arabicNumeral)
 
 		then:
-		romanNumeral == expectedRomanNumeral
+		romanNumeral == expectedResult
 
 		where:
-		arabicNumeral | expectedRomanNumeral
-		1             | "I"
-		2             | "II"
-		3             | "III"
-		4             | "IV"
-		5             | "V"
-		6             | "VI"
-		7             | "VII"
-		8             | "VIII"
-		9             | "IX"
-		10            | "X"
-		11            | "XI"
-
+		arabicNumeral || expectedResult
+		1             || "I"
+		2             || "II"
+		3             || "III"
+		4             || "IV"
+		5             || "V"
+		6             || "VI"
+		7             || "VII"
+		8             || "VIII"
+		9             || "IX"
+		10            || "X"
+		11            || "XI"
+		12            || "XII"
+		13            || "XIII"
+		14            || "XIV"
+		15            || "XV"
+		16            || "XVI"
+		17            || "XVII"
+		18            || "XVIII"
+		19            || "XIX"
+		20            || "XX"
 	}
 
 	def transform(def arabicNumeral) {
-		def correspondence = arabicToRoman[arabicNumeral]
-		if (correspondence) {
-			return correspondence
+		def romanCorespondent = arabicToRoman[arabicNumeral]
+		if (romanCorespondent) {
+			return romanCorespondent
+		}  else if (arabicNumeral / 10 >= 1) {
+			def remainder = arabicNumeral % 10
+			return arabicToRoman[10] + transform(remainder)
 		}
-			else if(arabicNumeral / 10 > 0 ) {
-				return arabicToRoman[10] + arabicToRoman[arabicNumeral % 10]
-		} else {
-			return arabicToRoman[5] + arabicToRoman[arabicNumeral % 5]
+		else {
+			def remainder = arabicNumeral % 5
+			def romanRemainder = arabicToRoman[remainder]
+			return arabicToRoman[5] + romanRemainder
 		}
 	}
 }
