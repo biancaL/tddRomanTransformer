12ea454a5e03988940fb63f0e2e63ddc31b7e310
Bianca Leuca <bianca.leuca@gmail.com>
2017-06-06 14:11:49 +0300

    Implemented kata

--- src/test/groovy/RomanNumeralTransformerSpec.groovy
+++ src/test/groovy/RomanNumeralTransformerSpec.groovy
@@ -1,19 +1,24 @@
-import groovy.util.logging.Slf4j
 import spock.lang.Specification
 import spock.lang.Unroll
 
-@Slf4j
 class RomanNumeralTransformerSpec extends Specification {
+
+	def arabicToRoman = [1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"]
+
+	def "it should pass"() {
+		expect:
+		true
+	}
 
 	@Unroll
 	def "arabic numeral #arabicNumeral should return roman numeral #expectedRomanNumeral"() {
-		when: "Arabic numeral is transformed to roman numeral"
-		def result = new NumeralTransformer().transform(arabicNumeral)
+		when:"Tranformer method is applied on arabicNumeral"
+		def result = transform(arabicNumeral)
 
-		then: "Result is #expectedRomanNumeral"
+		then:"Result is #expectedRomanNumeral"
 		result == expectedRomanNumeral
 
-		where: "Arabic numeral #arabicNumeral"
+		where:"Arabic numeral is #arabicNumeral"
 		arabicNumeral | expectedRomanNumeral
 		1             | "I"
 		2             | "II"
@@ -26,42 +31,45 @@ class RomanNumeralTransformerSpec extends Specification {
 		9             | "IX"
 		10            | "X"
 		11            | "XI"
-		15            | "XV"
+		14            | "XIV"
 		20            | "XX"
-		30            | "XXX"
-		40            | "LC"
-		41            | "LCI"
-		3789          | "MMMDCCLXXXIX"
+		36            | "XXXVI"
+		40            | "XL"
+		41            | "XLI"
+		51            | "LI"
+		91            | "XCI"
+		100           | "C"
+		400           | "CD"
+		500           | "D"
+		900           | "CM"
+		1000          | "M"
+		4765          | "MMMMDCCLXV"
+
 	}
-
-	def "throws exception for arabicNumeral #arabicNumeral"() {
-		when: "Arabic numeral is transformed"
-		new NumeralTransformer().transform(arabicNumeral)
-
-		then: "IllegalArgumentException is thrown"
-		thrown(IllegalArgumentException)
-
-		where: "Arabic numeral is"
-		arabicNumeral << [0, -1, 5000]
-	}
-
-	def "returns sum between #firstRoman and #secondRoman"() {
-		given:
-		def operations = new RomanOperations()
-		operations.numeralTranformer = Mock(NumeralTransformer)
-
-
+	@Unroll
+	def "throws IllegalArgumentException when arabicNumeral is #arabicNumeral"() {
 		when:
-		def result = operations.sum(firstRoman, secondRoman)
+		transform(arabicNumeral)
 
 		then:
-		1 * operations.numeralTranformer.transformToArabic(firstRoman) >> 1
-		1 * operations.numeralTranformer.transformToArabic(secondRoman) >> 2
-		1 * operations.numeralTranformer.transform(3) >> "III"
-		result == expectedSum
+		thrown(IllegalArgumentException)
 
 		where:
-		firstRoman | secondRoman || expectedSum
-		"I"        | "II"         | "III"
+		arabicNumeral <<[0, -1, 5000]
+
+	}
+
+	def transform(arabicNumeral) {
+		if(arabicNumeral < 1 || arabicNumeral > 4999) {
+			throw new IllegalArgumentException()
+		}
+		if (arabicToRoman[arabicNumeral] != null) {
+			return arabicToRoman[arabicNumeral]
+		}
+		for (def key : arabicToRoman.keySet().toList().reverse()) {
+			if (arabicNumeral > key) {
+				return arabicToRoman[key] + transform(arabicNumeral - key)
+			}
+		}
 	}
 }
